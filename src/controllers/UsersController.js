4d1019c1442dfa807d1295f8523cd1330cccb636
Natalye <cavalieridev@gmail.com>
2023-12-14 16:32:58 +0100

    created update users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -20,5 +20,32 @@ class UsersController {
 
     return response.status(201).json()
   }
+
+  async update (request, response){
+    const {name, email} = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    if(!user){
+      throw new AppError ("User not is exist")
+    }
+    const userWithUpdateEmail = await database.get
+    ("SELECT * FROM users WHERE email = (?)",[email])
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+      throw new AppError("This email already exist")
+    }
+    user.name = name;
+    user.email = email;
+    await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, new Date(), id]
+      );
+      return response.json()
+  }
 }
 module.exports = UsersController
