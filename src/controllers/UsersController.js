cb2dbd941e5b2c35b702b7861e82c0022b4529ab
Natalye <cavalieridev@gmail.com>
2023-12-14 17:34:39 +0100

    created update password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,17 @@
-const { hash } = require("bcryptjs")
-const AppError= require("../utils/AppError")
-const sqliteConnection= require("../database/sqlite")
+const { hash, compare } = require("bcryptjs")
+const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
     const database = await sqliteConnection()
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
 
-    if(checkUserExists){
-      throw new AppError ("This email already exist");
+    if (checkUserExists) {
+      throw new AppError("This email already exist")
     }
 
     const hashedPassword = await hash(password, 8)
@@ -21,31 +24,50 @@ class UsersController {
     return response.status(201).json()
   }
 
-  async update (request, response){
-    const {name, email} = request.body
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
-    if(!user){
-      throw new AppError ("User not is exist")
+    if (!user) {
+      throw new AppError("User not is exist")
     }
-    const userWithUpdateEmail = await database.get
-    ("SELECT * FROM users WHERE email = (?)",[email])
-    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id){
+    const userWithUpdateEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
       throw new AppError("This email already exist")
     }
-    user.name = name;
-    user.email = email;
-    await database.run(`
+    user.name = name
+    user.email = email
+
+    if (password && !old_password) {
+      throw new AppError("You need say the old password")
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError("The old password not confer")
+      }
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      `
       UPDATE users SET
       name = ?,
       email = ?,
+      password = ?,
       updated_at = ?
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
-      );
-      return response.json()
+      [user.name, user.email, user.password, new Date(), id]
+    )
+    return response.json()
   }
 }
 module.exports = UsersController
