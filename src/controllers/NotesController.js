ae7f100ce4fc7292141f76506f61477752eb5242
Natalye <cavalieridev@gmail.com>
2023-12-15 20:10:10 +0100

    created index notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,4 +1,4 @@
-const { request, response } = require("express");
+const { request, response } = require("express")
 const knex = require("../database/knex")
 
 class NotesController {
@@ -13,33 +13,60 @@ class NotesController {
       user_id,
     })
 
-    const tagsInsert = tags.map(name => {
-      return{
+    const tagsInsert = tags.map((name) => {
+      return {
         notes_id,
         name,
         user_id,
-    }
-    });
+      }
+    })
 
-    await  knex ("movie_tags").insert(tagsInsert)
+    await knex("movie_tags").insert(tagsInsert)
     response.json()
   }
 
-  async show(request, response){
+  async show(request, response) {
     const { id } = request.params
     const note = await knex("movie_notes").where({ id }).first()
-    const tags = await knex("movie_tags").where({notes_id: id}).orderBy("name")
+    const tags = await knex("movie_tags")
+      .where({ notes_id: id })
+      .orderBy("name")
     return response.json({
       ...note,
-      tags
+      tags,
     })
   }
 
-  async delete(request, response){
+  async delete(request, response) {
     const { id } = request.params
     await knex("movie_notes").where({ id }).delete()
 
     return response.json()
+  }
+
+  async index(request, response) {
+    const { user_id, title, tags } = request.query
+
+    let notes
+    if (tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim())
+      notes = await knex("movie_tags")
+      .select([
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.user_id",
+      ])
+      .where("movie_notes.user_id", user_id)
+      .whereLike("movie_notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movie_notes", "movie_notes.id", "movie_tags.notes_id")
+    } else {
+      notes = await knex("movie_notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title")
+    }
+    return response.json(notes)
   }
 }
 module.exports = NotesController
--- src/database/knex/migrations/20231214172940_createTags.js
+++ src/database/knex/migrations/20231214172940_createTags.js
@@ -1,9 +1,10 @@
 
 exports.up = knex => knex.schema.createTable("movie_tags", table =>{
   table.increments("id");
+  table.text("name").notNullable()
   table.integer("notes_id").references("id").inTable("movie_notes").onDelete("CASCADE")
   table.integer("user_id").references("id").inTable("users")
-  table.text("name").notNullable()
+  
 
 });
 
