69c89f568bf8bde90b33e8ef5618291755be03ca
Natalye <cavalieridev@gmail.com>
2024-01-23 10:27:21 +0100

    installed and configurated pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const uploadConfig = require("./configs/upload")
 const migrationsRun = require("./database/sqlite/migrations")
@@ -31,5 +32,5 @@ app.use((error, request, response, next) => {
   
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
